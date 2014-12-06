32a04bd18b44d5ae2dc3a9a0cfd4490461fbf307
jnphilipp <jnphilipp@gmail.com>
2014-12-06 17:39:43 +0100

    Added model accounts.models.Category.

---
+++ accounts/migrations/0002_category.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import accounts.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('name', accounts.models.TextFieldSingleLine(unique=True)),
+                ('slug', models.SlugField(unique=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Categories',
+                'ordering': ('name',),
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- accounts/models.py
+++ accounts/models.py
@@ -26,3 +26,26 @@ class Unit(models.Model):
 
 	class Meta:
 		ordering = ('name',)
+
+class Category(models.Model):
+	updated_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	name = TextFieldSingleLine(unique=True)
+	slug = models.SlugField(unique=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.name)
+		else:
+			orig = Category.objects.get(pk=self.id)
+			if orig.name != self.name:
+				self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name_plural = 'Categories'
