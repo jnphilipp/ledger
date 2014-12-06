dad2f727fe8d620348608546e9a82e735f42200a
jnphilipp <jnphilipp@gmail.com>
2014-12-06 17:31:00 +0100

    Updated settings.

--- ledger/settings.py
+++ ledger/settings.py
@@ -8,76 +8,98 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from configparser import RawConfigParser
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 import os
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+config = RawConfigParser()
+config.optionxform = str
+config.read(BASE_DIR + '/ledger/settings.ini')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3v+c8-nybtllych5k(#+dtkb9yti6o$4nj@2l$ayfel0rbfi@o'
+SECRET_KEY = config.get('secrets','SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.getboolean('debug','DEBUG')
+TEMPLATE_DEBUG = config.getboolean('debug','TEMPLATE_DEBUG')
 
-TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = config.get('host','ALLOWED_HOSTS').split()
+ADMINS = tuple(config.items('admins'))
 
-ALLOWED_HOSTS = []
-
+EMAIL_USE_TLS = config.getboolean('host_email','EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = config.get('host_email','DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = config.get('host_email','SERVER_EMAIL')
+EMAIL_HOST = config.get('host_email','EMAIL_HOST')
+EMAIL_PORT = config.getint('host_email','EMAIL_PORT')
+EMAIL_HOST_USER = config.get('host_email','EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config.get('host_email','EMAIL_HOST_PASSWORD')
 
 # Application definition
-
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'autocomplete_light',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'ledger.urls'
-
 WSGI_APPLICATION = 'ledger.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': config.get('database', 'DATABASE_ENGINE'),
+		'NAME': os.path.join(BASE_DIR, config.get('database', 'DATABASE_NAME')),
+	}
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'assets'),
+)
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+# Templates
+TEMPLATE_DIRS = (
+	os.path.join(BASE_DIR, 'templates'),
+)
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+)
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+	'django.core.context_processors.request',
+	'django.contrib.messages.context_processors.messages',
+)
