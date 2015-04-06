fe1cc78c69b74acd9e59f9ffa2fc086122c8f59a
jnphilipp <jnphilipp@gmail.com>
2015-04-06 12:01:57 +0200

    Added delete/edit to tag. #8

--- accounts/views/tag.py
+++ accounts/views/tag.py
@@ -1,4 +1,5 @@
 from accounts.charts import tag_chart
+from accounts.forms import TagForm
 from accounts.models import Tag, Unit
 from app.models import Ledger
 from collections import OrderedDict
@@ -32,3 +33,29 @@ def entries(request, slug):
 	totals = {Unit.objects.get(pk=unit):round(sum(entry.amount for entry in tag.entries.filter(account__ledger=ledger).filter(account__unit=unit)), 2) for unit in set(tag.entries.filter(account__ledger=ledger).values_list('account__unit', flat=True))}
 	entries = tag.entries.filter(account__ledger=ledger).order_by('day').reverse()
 	return render(request, 'ledger/accounts/tag/entries.html', locals())
+
+@login_required(login_url='/login/')
+@csrf_protect
+def edit(request, slug):
+	tag = get_object_or_404(Tag, slug=slug)
+	if request.method == 'POST':
+		form = TagForm(instance=tag, data=request.POST)
+		if form.is_valid():
+			tag = form.save()
+			messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully updated.' % tag.name.lower())
+			return redirect('tag', slug=tag.slug)
+		else:
+			return render(request, 'ledger/accounts/tag/form.html', locals())
+	else:
+		form = TagForm(instance=tag)
+		return render(request, 'ledger/accounts/tag/form.html', locals())
+
+@login_required(login_url='/login/')
+@csrf_protect
+def delete(request, slug):
+	tag = get_object_or_404(Tag, slug=slug)
+	if request.method == 'POST':
+		tag.delete()
+		messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully deleted.' % tag.name.lower())
+		return redirect('tags')
+	return render(request, 'ledger/accounts/tag/delete.html', locals())
