a9aa968184da6d87b0b661abe4e35b4c97f77a87
jnphilipp <me@jnphilipp.org>
2015-06-27 21:35:20 +0200

    Updated tags templates and views.

---
+++ accounts/views/api/tags/charts.py
@@ -0,0 +1,50 @@
+from accounts.models import Account, Tag, Unit
+from app.models import Ledger
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q, Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from json import dumps
+
+@login_required(login_url='/login/')
+def statistics(request, slug):
+    year = request.GET.get('year')
+    tag = get_object_or_404(Tag, slug=slug)
+    ledger = get_object_or_404(Ledger, user=request.user)
+    units = Unit.objects.filter(id__in=set(tag.entries.filter(account__ledgers=ledger).values_list('account__unit', flat=True)))
+
+    if year:
+        months = tag.entries.filter(Q(account__ledger=ledger) & Q(day__year=year)).dates('day', 'month')
+        data = {}
+        data['xAxis'] = {'categories':[m.strftime('%B') for m in months], 'title':'months'}
+        data['yAxis'] = {'stackLabels':{'format':'{total:,.2f}%s' % (units[0].symbol if units.count() == 1 else '')}, 'labels':{'format':'{value}%s' % (units[0].symbol if units.count() == 1 else '')}}
+        data['tooltip'] = {'valueSuffix':(units[0].symbol if units.count() == 1 else '')}
+
+        series = []
+        for account in Account.objects.filter(Q(entry__tags=tag) & Q(ledgers=ledger) & Q(entry__day__year=year)).distinct():
+            series.append({'name':account.name.lower(), 'data':[[m.strftime('%B'), tag.entries.filter(Q(account=account) & Q(day__year=year) & Q(day__month=m.strftime('%m'))).aggregate(sum=Sum('amount'))['sum']] for m in months], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
+
+        for unit in units:
+            avg = tag.entries.filter(Q(account__ledgers=ledger) & Q(account__unit=unit) & Q(day__year=year)).aggregate(sum=Sum('amount'))['sum'] / len(months)
+            series.append({'name':'average %s' % unit.name.lower(), 'type':'spline', 'data':[avg for m in months], 'tooltip':{'valueSuffix':unit.symbol}})
+        data['series'] = series
+    else:
+        years = tag.entries.filter(account__ledger=ledger).dates('day', 'year')
+        data = {}
+        data['xAxis'] = {'categories':[y.strftime('%Y') for y in years], 'title':'years'}
+        data['yAxis'] = {'stackLabels':{'format':'{total:,.2f}%s' % (units[0].symbol if units.count() == 1 else '')}, 'labels':{'format':'{value}%s' % (units[0].symbol if units.count() == 1 else '')}}
+        data['tooltip'] = {'valueSuffix':(units[0].symbol if units.count() == 1 else '')}
+
+        series = []
+
+        for account in Account.objects.filter(Q(entry__tags=tag) & Q(ledgers=ledger) & Q()).distinct():
+            series.append({'name':account.name.lower(), 'data':[[y.strftime('%Y'), tag.entries.filter(Q(account=account) & Q(day__year=y.strftime('%Y'))).aggregate(sum=Sum('amount'))['sum']] for y in years], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
+
+        for unit in units:
+            avg = tag.entries.filter(Q(account__ledgers=ledger) & Q(account__unit=unit)).aggregate(sum=Sum('amount'))['sum'] / len(years)
+            series.append({'name':'average %s' % unit.name.lower(), 'type':'spline', 'data':[avg for y in years], 'tooltip':{'valueSuffix':unit.symbol}})
+
+        data['series'] = series
+
+    mimetype = 'application/json'
+    return HttpResponse(dumps(data), mimetype)
--- accounts/views/tag.py
+++ accounts/views/tag.py
@@ -1,5 +1,5 @@
 from accounts.charts import tag_chart
-from accounts.forms import TagForm
+from accounts.forms import TagForm, TagFilterForm
 from accounts.models import Tag, Unit
 from app.models import Ledger
 from collections import OrderedDict
@@ -13,49 +13,89 @@ from json import dumps
 
 @login_required(login_url='/login/')
 def tags(request):
-	ledger = get_object_or_404(Ledger, user=request.user)
-	tags = Tag.objects.filter(entries__account__ledger=ledger).distinct().extra(select={'lname':'lower(accounts_tag.name)'}).order_by('lname')
-	return render(request, 'ledger/accounts/tag/tags.html', locals())
+    ledger = get_object_or_404(Ledger, user=request.user)
+    tags = Tag.objects.filter(entries__account__ledger=ledger).distinct().extra(select={'lname':'lower(accounts_tag.name)'}).order_by('lname')
+
+    if request.method == 'POST':
+        form = TagFilterForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['accounts']:
+                tags = tags.filter(entries__account__in=form.cleaned_data['accounts'])
+            if form.cleaned_data['categories']:
+                tags = tags.filter(entries__category__in=form.cleaned_data['categories'])
+            if form.cleaned_data['tags']:
+                tags = tags.filter(id__in=form.cleaned_data['tags'])
+    else:
+        form = TagFilterForm()
+
+    return render(request, 'ledger/accounts/tag/tags.html', locals())
 
 @login_required(login_url='/login/')
 def tag(request, slug):
-	ledger = get_object_or_404(Ledger, user=request.user)
-	tag = get_object_or_404(Tag, slug=slug)
-	totals = {Unit.objects.get(pk=unit):round(sum(entry.amount for entry in tag.entries.filter(account__ledger=ledger).filter(account__unit=unit)), 2) for unit in set(tag.entries.filter(account__ledger=ledger).values_list('account__unit', flat=True))}
-	entries = tag.entries.filter(account__ledger=ledger).order_by('day').reverse()[:5]
-	monthly_data, yearly_data, ml, yl = tag_chart(tag, ledger)
-	return render(request, 'ledger/accounts/tag/tag.html', locals())
+    ledger = get_object_or_404(Ledger, user=request.user)
+    tag = get_object_or_404(Tag, slug=slug)
+    year = request.GET.get('year')
+
+    totals = {Unit.objects.get(pk=unit):round(sum(entry.amount for entry in tag.entries.filter(account__ledger=ledger).filter(account__unit=unit)), 2) for unit in set(tag.entries.filter(account__ledger=ledger).values_list('account__unit', flat=True))}
+    entries = tag.entries.filter(account__ledger=ledger).order_by('day').reverse()[:5]
+
+    if not year:
+        years = [y.strftime('%Y') for y in tag.entries.filter(account__ledger=ledger).dates('day', 'year')]
+    return render(request, 'ledger/accounts/tag/tag.html', locals())
 
 @login_required(login_url='/login/')
 def entries(request, slug):
-	ledger = get_object_or_404(Ledger, user=request.user)
-	tag = get_object_or_404(Tag, slug=slug)
-	totals = {Unit.objects.get(pk=unit):round(sum(entry.amount for entry in tag.entries.filter(account__ledger=ledger).filter(account__unit=unit)), 2) for unit in set(tag.entries.filter(account__ledger=ledger).values_list('account__unit', flat=True))}
-	entries = tag.entries.filter(account__ledger=ledger).order_by('day').reverse()
-	return render(request, 'ledger/accounts/tag/entries.html', locals())
+    ledger = get_object_or_404(Ledger, user=request.user)
+    tag = get_object_or_404(Tag, slug=slug)
+    totals = {Unit.objects.get(pk=unit):round(sum(entry.amount for entry in tag.entries.filter(account__ledger=ledger).filter(account__unit=unit)), 2) for unit in set(tag.entries.filter(account__ledger=ledger).values_list('account__unit', flat=True))}
+
+    if request.method == 'POST':
+        form = TagFilterForm(request.POST)
+        entries = tag.entries.all().reverse()
+        if form.is_valid():
+            if form.cleaned_data['accounts']:
+                entries = entries.filter(account__in=form.cleaned_data['accounts'])
+            if form.cleaned_data['categories']:
+                entries = entries.filter(category__in=form.cleaned_data['categories'])
+            if form.cleaned_data['tags']:
+                entries = entries.filter(tags__in=form.cleaned_data['tags'])
+    else:
+        form = TagFilterForm()
+        entries = tag.entries.filter(account__ledger=ledger).order_by('day').reverse()
+    return render(request, 'ledger/accounts/tag/entries.html', locals())
+
+@login_required(login_url='/login/')
+def statistics(request, slug):
+    ledger = get_object_or_404(Ledger, user=request.user)
+    tag = get_object_or_404(Tag, slug=slug)
+    year = request.GET.get('year')
+
+    if not year:
+        years = [y.strftime('%Y') for y in tag.entries.filter(account__ledger=ledger).dates('day', 'year')]
+    return render(request, 'ledger/accounts/tag/statistics.html', locals())
 
 @login_required(login_url='/login/')
 @csrf_protect
 def edit(request, slug):
-	tag = get_object_or_404(Tag, slug=slug)
-	if request.method == 'POST':
-		form = TagForm(instance=tag, data=request.POST)
-		if form.is_valid():
-			tag = form.save()
-			messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully updated.' % tag.name.lower())
-			return redirect('tag', slug=tag.slug)
-		else:
-			return render(request, 'ledger/accounts/tag/form.html', locals())
-	else:
-		form = TagForm(instance=tag)
-		return render(request, 'ledger/accounts/tag/form.html', locals())
+    tag = get_object_or_404(Tag, slug=slug)
+    if request.method == 'POST':
+        form = TagForm(instance=tag, data=request.POST)
+        if form.is_valid():
+            tag = form.save()
+            messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully updated.' % tag.name.lower())
+            return redirect('tag', slug=tag.slug)
+        else:
+            return render(request, 'ledger/accounts/tag/form.html', locals())
+    else:
+        form = TagForm(instance=tag)
+        return render(request, 'ledger/accounts/tag/form.html', locals())
 
 @login_required(login_url='/login/')
 @csrf_protect
 def delete(request, slug):
-	tag = get_object_or_404(Tag, slug=slug)
-	if request.method == 'POST':
-		tag.delete()
-		messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully deleted.' % tag.name.lower())
-		return redirect('tags')
-	return render(request, 'ledger/accounts/tag/delete.html', locals())
+    tag = get_object_or_404(Tag, slug=slug)
+    if request.method == 'POST':
+        tag.delete()
+        messages.add_message(request, messages.SUCCESS, 'the tag %s was successfully deleted.' % tag.name.lower())
+        return redirect('tags')
+    return render(request, 'ledger/accounts/tag/delete.html', locals())
