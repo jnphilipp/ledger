0a1eb8203346d87df90f3807ced3cba025dad2c7
jnphilipp <me@jnphilipp.org>
2015-06-29 21:21:34 +0200

    Updated dashboard.

--- accounts/templatetags/accounts/base.py
+++ accounts/templatetags/accounts/base.py
@@ -21,12 +21,12 @@ def colorfy(amount, currency=None, autoescape=None):
 @register.filter(needs_autoescape=True)
 def balance(account, autoescape=None):
     balance = sum(entry.amount for entry in account.entry_set.filter(day__lte=date.today()))
-    return colorfy(balance, account.unit)
+    return colorfy(balance, account.unit) if autoescape else balance
 
 @register.filter(needs_autoescape=True)
 def outstanding(account, autoescape=None):
     outstanding = sum(entry.amount for entry in account.entry_set.filter(day__gt=date.today()).filter(day__lte=get_last_date_current_month()))
-    return colorfy(outstanding, account.unit)
+    return colorfy(outstanding, account.unit) if autoescape else outstanding
 
 @register.filter
 def accounts(obj, user):
@@ -36,3 +36,17 @@ def accounts(obj, user):
         return Account.objects.filter(Q(entry__in=obj.entries.all()) & Q(ledgers__user=user)).distinct()
     else:
         return []
+
+@register.filter(needs_autoescape=True)
+def sumbalance(unit, user, autoescape=None):
+    sum_balance = 0
+    for account in Account.objects.filter(Q(unit=unit) & Q(ledgers__user=user)):
+        sum_balance += balance(account)
+    return colorfy(sum_balance, unit)
+
+@register.filter(needs_autoescape=True)
+def sumoutstanding(unit, user, autoescape=None):
+    sum_outstanding = 0
+    for account in Account.objects.filter(Q(unit=unit) & Q(ledgers__user=user)):
+        sum_outstanding += outstanding(account)
+    return colorfy(sum_outstanding, unit)
