1282e413b03b6b4b5666f1f6114f3ca5ee61b286
jnphilipp <me@jnphilipp.org>
2015-08-14 07:54:37 +0200

    Updated category.

--- accounts/views/api/categories/charts.py
+++ accounts/views/api/categories/charts.py
@@ -11,25 +11,25 @@ def statistics(request, slug):
     year = request.GET.get('year')
     category = get_object_or_404(Category, slug=slug)
     ledger = get_object_or_404(Ledger, user=request.user)
-    units = Unit.objects.filter(id__in=set(category.entry_set.filter(account__ledgers=ledger).values_list('account__unit', flat=True)))
+    units = Unit.objects.filter(id__in=set(category.entries.filter(account__ledgers=ledger).values_list('account__unit', flat=True)))
 
     if year:
-        months = category.entry_set.filter(Q(account__ledger=ledger) & Q(day__year=year)).dates('day', 'month')
+        months = category.entries.filter(Q(account__ledger=ledger) & Q(day__year=year)).dates('day', 'month')
         data = {}
         data['xAxis'] = {'categories':[m.strftime('%B') for m in months], 'title':'months'}
         data['yAxis'] = {'stackLabels':{'format':'{total:,.2f}%s' % (units[0].symbol if units.count() == 1 else '')}, 'labels':{'format':'{value}%s' % (units[0].symbol if units.count() == 1 else '')}}
         data['tooltip'] = {'valueSuffix':(units[0].symbol if units.count() == 1 else '')}
 
         series = []
-        for account in Account.objects.filter(Q(entry__category=category) & Q(ledgers=ledger) & Q(entry__day__year=year)).distinct():
-            series.append({'name':account.name.lower(), 'data':[[m.strftime('%B'), category.entry_set.filter(Q(account=account) & Q(day__year=year) & Q(day__month=m.strftime('%m'))).aggregate(sum=Sum('amount'))['sum']] for m in months], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
+        for account in Account.objects.filter(Q(entries__category=category) & Q(ledgers=ledger) & Q(entries__day__year=year)).distinct():
+            series.append({'name':account.name.lower(), 'data':[[m.strftime('%B'), category.entries.filter(Q(account=account) & Q(day__year=year) & Q(day__month=m.strftime('%m'))).aggregate(sum=Sum('amount'))['sum']] for m in months], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
 
         for unit in units:
-            avg = category.entry_set.filter(Q(account__ledgers=ledger) & Q(account__unit=unit) & Q(day__year=year)).aggregate(sum=Sum('amount'))['sum'] / len(months)
+            avg = category.entries.filter(Q(account__ledgers=ledger) & Q(account__unit=unit) & Q(day__year=year)).aggregate(sum=Sum('amount'))['sum'] / len(months)
             series.append({'name':'average %s' % unit.name.lower(), 'type':'spline', 'data':[avg for m in months], 'tooltip':{'valueSuffix':unit.symbol}})
         data['series'] = series
     else:
-        years = category.entry_set.filter(account__ledger=ledger).dates('day', 'year')
+        years = category.entries.filter(account__ledger=ledger).dates('day', 'year')
         data = {}
         data['xAxis'] = {'categories':[y.strftime('%Y') for y in years], 'title':'years'}
         data['yAxis'] = {'stackLabels':{'format':'{total:,.2f}%s' % (units[0].symbol if units.count() == 1 else '')}, 'labels':{'format':'{value}%s' % (units[0].symbol if units.count() == 1 else '')}}
@@ -37,11 +37,11 @@ def statistics(request, slug):
 
         series = []
 
-        for account in Account.objects.filter(Q(entry__category=category) & Q(ledgers=ledger)).distinct():
-            series.append({'name':account.name.lower(), 'data':[[y.strftime('%Y'), category.entry_set.filter(Q(account=account) & Q(day__year=y.strftime('%Y'))).aggregate(sum=Sum('amount'))['sum']] for y in years], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
+        for account in Account.objects.filter(Q(entries__category=category) & Q(ledgers=ledger)).distinct():
+            series.append({'name':account.name.lower(), 'data':[[y.strftime('%Y'), category.entries.filter(Q(account=account) & Q(day__year=y.strftime('%Y'))).aggregate(sum=Sum('amount'))['sum']] for y in years], 'tooltip':{'valueSuffix':account.unit.symbol}, 'type':'column', 'stack':account.unit.name.lower()})
 
         for unit in units:
-            avg = category.entry_set.filter(Q(account__ledgers=ledger) & Q(account__unit=unit)).aggregate(sum=Sum('amount'))['sum'] / len(years)
+            avg = category.entries.filter(Q(account__ledgers=ledger) & Q(account__unit=unit)).aggregate(sum=Sum('amount'))['sum'] / len(years)
             series.append({'name':'average %s' % unit.name.lower(), 'type':'spline', 'data':[avg for y in years], 'tooltip':{'valueSuffix':unit.symbol}})
 
         data['series'] = series
