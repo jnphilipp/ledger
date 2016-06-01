b0f49a40bb2917c02a25ba6deb06e4b893bc14ee
jnphilipp <me@jnphilipp.org>
2016-06-01 18:56:23 +0200

    Update ledger app.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,22 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from accounts.views import account
-from app.views import dashboard
+"""ledger URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
-from django.views import generic, static
+from django.views import static
 
 admin.site.site_header = 'ledger administration'
 
-urlpatterns = [
-    url(r'^$', dashboard.dashboard, name='dashboard'),
-    url(r'^accounts/', include('accounts.urls')),
-    url(r'^profile/', include('app.urls')),
 
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^autocomplete/', include('autocomplete_light.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += [url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT})]
