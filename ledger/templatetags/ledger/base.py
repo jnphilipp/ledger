96ce68f080221e2b560fabb0eb7e0754638806e0
jnphilipp <me@jnphilipp.org>
2016-06-07 14:41:44 +0200

    Update ledger templatetags.

--- ledger/templatetags/ledger/base.py
+++ ledger/templatetags/ledger/base.py
@@ -17,12 +17,13 @@ from . import register
 # def mod(x, y):
 #     return x % y
 
-# @register.filter
-# def lookup(d, key):
-#     if type(d) == dict:
-#         return d[key] if key in d else None
-#     else:
-#         return d[key] if key < len(d) else None
+
+@register.filter
+def get_item(d, key):
+    if type(d) == dict:
+        return d[key] if key in d else None
+    else:
+        return d[key] if key < len(d) else None
 
 
 @register.filter
@@ -30,20 +31,20 @@ def startswith(value, start):
     return value.startswith(start)
 
 
-# @register.filter
-# def previous(value, arg):
-#     try:
-#         return value[int(arg) - 1] if int(arg) - 1 != -1 else None
-#     except:
-#         return None
+@register.filter
+def previous(value, arg):
+    try:
+        return value[int(arg) - 1] if int(arg) - 1 != -1 else None
+    except:
+        return None
 
 
-# @register.filter
-# def next(value, arg):
-#     try:
-#         return value[int(arg) + 1]
-#     except:
-#         return None
+@register.filter
+def next(value, arg):
+    try:
+        return value[int(arg) + 1]
+    except:
+        return None
 
 
 # @register.filter(name='addcss')
