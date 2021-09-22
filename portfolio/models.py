14d0ce673aedceed84b72fc7a031bb0902e9582b
J. Nathanael Philipp <nathanael@philipp.land>
2021-09-23 00:41:20 +0200

    Add Stock model to portfolio app.

--- portfolio/models.py
+++ portfolio/models.py
@@ -16,7 +16,61 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with ledger.  If not, see <http://www.gnu.org/licenses/>.
+"""Portfolio Django app models."""
 
+from units.models import Unit
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Stock(models.Model):
+    """Stock ORM Model."""
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated at"))
+
+    slug = models.SlugField(unique=True, verbose_name=_("Slug"))
+    name = models.CharField(max_length=1024, unique=True, verbose_name=_("Name"))
+    isin = models.CharField(max_length=12, unique=True, verbose_name=_("ISIN"))
+    wkn = models.CharField(
+        max_length=6, unique=True, blank=True, null=True, verbose_name=_("WKN")
+    )
+    symbol = models.CharField(
+        max_length=12, unique=True, blank=True, null=True, verbose_name=_("Symbol")
+    )
+    currency = models.ForeignKey(
+        Unit,
+        models.SET_NULL,
+        related_name="stocks",
+        blank=True,
+        null=True,
+        verbose_name=_("Currency"),
+    )
+    traded = models.BooleanField(default=True, verbose_name=_("Traded"))
+
+    def get_absolute_url(self):
+        """Get absolute URL."""
+        return reverse_lazy("portfolio:stock_detail", args=[self.stock.slug])
+
+    def save(self, *args, **kwargs):
+        """Save."""
+        if not self.slug:
+            self.slug = slugify(f"{self.name} {self.isin}")
+        else:
+            orig = Stock.objects.get(pk=self.id)
+            if orig.name != self.name:
+                self.slug = slugify(f"{self.name} {self.isin}")
+        super(Stock, self).save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        """Name."""
+        return f"{self.name} [{self.symbol}]"
+
+    class Meta:
+        """Meta."""
+
+        ordering = ("name",)
+        verbose_name = _("Stock")
+        verbose_name_plural = _("Stocks")
