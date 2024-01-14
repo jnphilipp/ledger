78e3f961dd661f132c445eac57a0b730922dba06
J. Nathanael Philipp <nathanael@philipp.land>
2024-01-14 15:53:47 +0100

    Minor updates to license/copyright year in some files.

--- ledger/ledger/__init__.py
+++ ledger/ledger/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
-# Copyright (C) 2014-2023 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
+# Copyright (C) 2014-2024 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
 #
 # This file is part of ledger.
 #
@@ -19,7 +17,7 @@
 """Ledger Django app."""
 
 __author__ = "J. Nathanael Philipp"
-__copyright__ = "Copyright 2014-2023 J. Nathanael Philipp (jnphilipp)"
+__copyright__ = "Copyright 2014-2024 J. Nathanael Philipp (jnphilipp)"
 __license__ = "GPLv3+"
 __maintainer__ = __author__
 __email__ = "nathanael@philipp.land"
