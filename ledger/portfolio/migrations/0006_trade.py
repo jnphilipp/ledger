d940c617448316fec72b2ead1cc8266f62f6be0f
J. Nathanael Philipp <nathanael@philipp.land>
2024-09-04 09:26:38 +0200

    Update trade migration with new trade type option.

--- ledger/portfolio/migrations/0006_trade.py
+++ ledger/portfolio/migrations/0006_trade.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
-# Copyright (C) 2014-2021 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
+# Copyright (C) 2014-2024 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
 #
 # This file is part of ledger.
 #
@@ -25,33 +24,84 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('units', '0001_initial'),
-        ('portfolio', '0005_position'),
+        ("units", "0001_initial"),
+        ("portfolio", "0005_position"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Trade',
+            name="Trade",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created at')),
-                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Updated at')),
-                ('serial_number', models.IntegerField(verbose_name='Serial number')),
-                ('date', models.DateField(verbose_name='Date')),
-                ('units', models.FloatField(default=0., verbose_name='Units')),
-                ('unit_price', models.FloatField(default=0., verbose_name='Unit price')),
-                ('extra', models.FloatField(default=0., verbose_name='Extra costs')),
-                ('extra2', models.FloatField(default=0., verbose_name='Extra costs')),
-                ('exchange_rate', models.FloatField(blank=True, null=True, verbose_name='Exchange rate')),
-                ('type', models.IntegerField(choices=[(None, '(Unknown)'), (0, 'Buy'), (1, 'Sell'), (2, 'Dividend'), (3, 'Pre-emption right')], verbose_name='Type')),
-                ('position', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='trades', to='portfolio.position', verbose_name='Position')),
-                ('unit', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='trades', to='units.unit', verbose_name='Unit')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created_at",
+                    models.DateTimeField(auto_now_add=True, verbose_name="Created at"),
+                ),
+                (
+                    "updated_at",
+                    models.DateTimeField(auto_now=True, verbose_name="Updated at"),
+                ),
+                ("serial_number", models.IntegerField(verbose_name="Serial number")),
+                ("date", models.DateField(verbose_name="Date")),
+                ("units", models.FloatField(default=0.0, verbose_name="Units")),
+                (
+                    "unit_price",
+                    models.FloatField(default=0.0, verbose_name="Unit price"),
+                ),
+                ("extra", models.FloatField(default=0.0, verbose_name="Extra costs")),
+                ("extra2", models.FloatField(default=0.0, verbose_name="Extra costs")),
+                (
+                    "exchange_rate",
+                    models.FloatField(
+                        blank=True, null=True, verbose_name="Exchange rate"
+                    ),
+                ),
+                (
+                    "type",
+                    models.IntegerField(
+                        choices=[
+                            (None, "(Unknown)"),
+                            (0, "Buy"),
+                            (1, "Sell"),
+                            (2, "Dividend"),
+                            (3, "Pre-emption right"),
+                            (4, "Corporate action"),
+                        ],
+                        verbose_name="Type",
+                    ),
+                ),
+                (
+                    "position",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="trades",
+                        to="portfolio.position",
+                        verbose_name="Position",
+                    ),
+                ),
+                (
+                    "unit",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="trades",
+                        to="units.unit",
+                        verbose_name="Unit",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Trade',
-                'verbose_name_plural': 'Trades',
-                'ordering': ('position', '-serial_number'),
-                'unique_together': {('position', 'serial_number')},
+                "verbose_name": "Trade",
+                "verbose_name_plural": "Trades",
+                "ordering": ("position", "-serial_number"),
+                "unique_together": {("position", "serial_number")},
             },
         ),
     ]
